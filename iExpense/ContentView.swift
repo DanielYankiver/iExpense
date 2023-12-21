c00508493b0c9c90a81e80ae4838312590caec01
Daniel Yankiver <dyankiver@gmail.com>
2023-12-21 15:48:10 -0500

    built a test list where items can be deleted

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,18 +7,43 @@
 
 import SwiftUI
 
+struct ExpenseItem {
+  let name: String
+  let type: String
+  let amount: Double
+}
+
+@Observable
+class Expenses {
+  var items = [ExpenseItem]()
+}
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  @State private var expenses = Expenses()
+
+  var body: some View {
+    NavigationStack {
+      List {
+        ForEach(expenses.items, id: \.name) { item in
+          Text(item.name)
         }
-        .padding()
+        .onDelete(perform: removeItems)
+      }
+      .navigationTitle("iExpense")
+      .toolbar {
+        Button("Add Expense", systemImage: "plus") {
+          let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+          expenses.items.append(expense)
+        }
+      }
     }
+  }
+
+  func removeItems(at offsets: IndexSet) {
+    expenses.items.remove(atOffsets: offsets)
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
